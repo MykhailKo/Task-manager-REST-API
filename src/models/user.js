4ed24d8226c686e35e59074acd032ffec95bb554
MykhailKo <tuareg1812@gmail.com>
2020-08-28 19:11:34 +0300

    JWT authentication, setting express and mongoose middleware, User/Task rel

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -10,6 +12,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -32,12 +35,56 @@ const userSchema = new mongoose.Schema({
         validate(value){
             if(value < 0) throw new Error('Age must be a positive number.')
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
+userSchema.methods.toJSON = function() {
+    const userObject = this.toObject()
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
+}
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'ofcourseistillloveyou')
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if(!user) throw new Error('Unable to login')
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) throw new Error('Unable to login')
+
+    return user
+}
+
+// hashing password before saving 
 userSchema.pre('save', async function(next) {
     const user = this
     if(user.isModified('password')) user.password = await bcrypt.hash(user.password, 8)
+    next()
+})
+
+// deleting tasks when user is deleted
+userSchema.pre('remove', async function(next) {
+    await Task.deleteMany({ owner: this._id })
     next()
 })
 
